a291da3d601993a7b3b70bd7af7ad73c6b57a809
Jonathan Borda <stivborda@gmail.com>
2024-05-20 23:41:52 -0500

    Ultimos ajustes.

--- API_Biblioteca/obj/Debug/net8.0/API_Biblioteca.AssemblyInfo.cs
+++ API_Biblioteca/obj/Debug/net8.0/API_Biblioteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4ea9267f73b9339499b990e76b0e480dbd7f0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8626c24e04386d4ac85529f83957988f6bcef99c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
